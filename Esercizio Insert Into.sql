3029e01550580e243783be7f56b38cb5b029d37f
Maurilio Valenti <166375402+Maurilio93@users.noreply.github.com>
2024-08-28 16:25:21 +0200

    Add files via upload
    Esercizio '' insert into''

---
+++ Esercizio Insert Into.sql
@@ -0,0 +1,8 @@
+INSERT INTO book (titolo, autore, genere, anno_pubblicazione, codice_isbn, prezzo, valutazione, numero_scorte, editore, pagine)
+VALUES 
+('Un viaggio al centro', 'Jules Verne', 'Avventura', 1864, '1234567890123', 12.99, 4.5, 10, 'Verne Publishing', 350);
+('Guerra e pace', 'Lev Tolstoj', 'Storico', 1869, '1234567890124', 14.99, 4.8, 5, 'Tolstoj Prints', 1200);
+('Sussurri del vento', 'Amelia Blackburn', 'Romantico', 1982, '1234567890125', 9.99, 4.2, 20, 'Blackburn House', 275);
+('L''Odissea Galattica', 'Orion Starfield', 'Fantascienza', 2005, '1234567890126', 19.99, 4.9, 15, 'Nebula Press', 450);
+
+
